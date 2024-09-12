9b10324986e9b980f099eb62ed3482879e0310e5
yundii <yunditao143@gmail.com>
2024-09-12 12:05:46 -0700

    added a header component and passed appName as a prop

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./Components/Header";
 
 export default function App() {
-  const appName = "My awesome napp!";
+  const appName = "My awesome app!";
   return (
     <View style={styles.container}>
       <Text>Welcome to {appName}! </Text>
       <StatusBar style="auto" />
+      <Header name={appName}></Header>
     </View>
   );
 }
---
+++ Components/Header.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Header({name}) {
+  return (
+    <View>
+      <Text>Welcome to {name} !!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
