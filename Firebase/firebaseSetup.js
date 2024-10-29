a0749bb5bd2b73d3bdd1d9d4ed17224c79390509
yundii <yunditao143@gmail.com>
2024-10-29 00:04:25 -0700

    Initialize Authentication SDK

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth'
 
 
 // Your web app's Firebase configuration
@@ -15,3 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+export const auth = getAuth(app);
