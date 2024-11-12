4696f3eb733da60b187566f412df8dcb31997618
yundii <yunditao143@gmail.com>
2024-11-12 14:59:52 -0800

    show a marker when user pick a location on the map

--- Components/Map.js
+++ Components/Map.js
@@ -1,10 +1,15 @@
 import MapView from "react-native-maps";
 import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
+import { useState } from "react";
+import { Marker } from "react-native-maps";
+
 
 
 export default function Map() {
+    const [selectedLocation, setSelectedLocation] = useState(null);
     return <MapView 
+        onPress={(e) => setSelectedLocation({latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude})}
         style={styles.map}
         initialRegion={{
             latitude: 37.7749,
@@ -12,7 +17,9 @@ export default function Map() {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
         }}
-    />;
+>
+        {selectedLocation && <Marker coordinate={selectedLocation} title="Selected Location" description="Selected Location" />}
+</MapView>
 }
 
 const styles = StyleSheet.create({
