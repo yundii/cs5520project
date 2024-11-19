758c996d886668d9777ef814fbb579079dc01dda
yundii <yunditao143@gmail.com>
2024-11-19 13:28:50 -0800

    add a location button in map.js to navigate to profile

--- Components/Map.js
+++ Components/Map.js
@@ -1,17 +1,25 @@
 import MapView from "react-native-maps";
 import React from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, View , Button} from "react-native";
 import { useState } from "react";
 import { Marker } from "react-native-maps";
+import { useNavigation } from "@react-navigation/native";
 
 
 
 export default function Map() {
     const [selectedLocation, setSelectedLocation] = useState(null);
-    return <MapView 
-        onPress={(e) => setSelectedLocation({latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude})}
-        style={styles.map}
-        initialRegion={{
+    const navigation = useNavigation();
+    function confirmHandler() {
+        // navigate to profile and pass the selected location to the profile
+        navigation.navigate("Profile", {location: selectedLocation});
+    }
+    return (
+        <View>
+            <MapView 
+                onPress={(e) => setSelectedLocation({latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude})}
+                style={styles.map}
+                initialRegion={{
             latitude: 37.7749,
             longitude: -122.4194,
             latitudeDelta: 0.0922,
@@ -20,11 +28,14 @@ export default function Map() {
 >
         {selectedLocation && <Marker coordinate={selectedLocation} title="Selected Location" description="Selected Location" />}
 </MapView>
+ <Button disabled={!selectedLocation}  title="Confirm Location" onPress={(confirmHandler)} />
+ </View>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        width: "100%",
+        height: "90%",
     }
 });
